eedae4781e51f8751159c5ab330a2906a878ab3d
Eli Ennab <elinaennab@gmail.com>
2023-04-27 16:39:48 +0200

    delete todo and show message if there are no todos in the list

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -9,9 +9,9 @@ type Todo = {
 function App() {
 
 	const [todos, setTodos] = useState<Todo[]>([
-		{ title: "my first todo", completed: false },
-		{ title: "my second todo", completed: false },
-		{ title: "my third todo", completed: true }
+		{ title: "Learn React", completed: false },
+		{ title: "Learn UX", completed: false },
+		{ title: "Learn TypeScript", completed: true }
 	])
 
 	const [newTodoTitle, setNewTodoTitle] = useState("")
@@ -28,49 +28,62 @@ function App() {
 		setNewTodoTitle("")
 	}
 
+	const deleteTodo = (deleteTodo: Todo) => {
+		console.log("i want to delete this todo:", deleteTodo)
+		console.log("list before delete", todos)
+		setTodos(todos.filter(todos => todos !== deleteTodo))
+		console.log("list after delete", todos)
+	}
+
   return (
 	<div className="App">
 
-	<h1>Todos</h1>
+		<h1>Todos</h1>
 
-	<form>
-		<div className="input-group mb-3">
-			<input
-				type="text"
-				className="form-control"
-				placeholder="write your todo title here..."
-				onChange={(e) => { setNewTodoTitle(e.target.value) }}
-				value={newTodoTitle}
-				/>
-			<button
-				className="btn btn-outline-secondary"
-				type="button"
-				onClick={createTodo}
-				>Create todo
-			</button>
-		</div>
-	</form>
+		<form>
+			<div className="input-group mb-3">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Write the title of your todo here"
+					onChange={(e) => { setNewTodoTitle(e.target.value) }}
+					value={newTodoTitle}
+					/>
+				<button
+					className="btn btn-outline-secondary"
+					type="button"
+					onClick={createTodo}
+					>Create todo
+				</button>
+			</div>
+		</form>
 
+	{(todos.length > 0 &&
+			<ul className="list-group">
+				{
+					todos.map( (todo, index) =>
+						<li
+							key={index}
+							className="todo">
+							{todo.title} {todo.completed ? "✓" : "×"}
+							<button
+								className="btn btn-dark btn-toggle">
+								{!todo.completed ? "mark as completed" : "mark as not completed"}
+							</button>
+							<button
+								className="btn btn-grey btn-delete"
+								onClick={ () => (deleteTodo(todo)) }
+								>delete
+							</button>
+						</li>
+					)
+				}
+			</ul>
+		)}
 
-		<ul className="list-group">
-			{
-				todos.map( (todo, index) =>
-					<li
-						key={index}
-						className="todo">
-						{todo.title} {todo.completed ? "✓" : "×"}
-						<button
-							className="btn btn-dark btn-toggle">
-							{!todo.completed ? "mark as completed" : "mark as not completed"}
-						</button>
-						<button
-							className="btn btn-grey btn-delete">
-							delete
-						</button>
-					</li>
-				)
-			}
-		</ul>
+		{( todos.length === 0 &&
+			<p>You do not have any todos.</p>
+		)}
 	</div>
   )
 }
