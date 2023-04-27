cc07363e9cdc79bc5e317f8644d9113f75da070c
Eli Ennab <elinaennab@gmail.com>
2023-04-27 15:48:45 +0200

    default todos and deletebtn, map n' render them out to the dom

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -1,10 +1,42 @@
 import { useState } from 'react'
 import './App.css'
 
-// function App() {
-//   return (
+type Todo = {
+	title: string
+	completed: boolean
+}
 
-//   )
-// }
+function App() {
 
-// export default App
+	const [todos, setTodos] = useState<Todo[]>([
+		{ title: "my first todo", completed: false },
+		{ title: "my second todo", completed: false },
+		{ title: "my third todo", completed: true }
+	])
+
+  return (
+	<div className="App">
+
+	<h1>Todos</h1>
+
+
+		<ul className="list-group">
+			{
+				todos.map( (todo, index) =>
+					<li
+						key={index}
+						className="todo">
+						{todo.title}
+						<button
+							className="btn btn-dark">
+							delete
+					</button>
+					</li>
+				)
+			}
+		</ul>
+	</div>
+  )
+}
+
+export default App
