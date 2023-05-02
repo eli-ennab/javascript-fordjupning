7a4b027bab1147fe6887633c4d787795e14471ba
Eli Ennab <elinaennab@gmail.com>
2023-05-02 14:48:51 +0200

    a working digital clock

--- 03-clock/src/App.tsx
+++ 03-clock/src/App.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react'
 import './assets/scss/App.scss'
 
 function App() {
 
+	const [time, setTime] = useState(() => {
+		return new Date().toLocaleTimeString()
+	})
+
+	useEffect(() => {
+		console.log("Starting clock...")
+		setInterval(() => {
+			setTime(new Date().toLocaleTimeString())
+		}, 1000)
+		console.log("Tick")
+	}, [])
+
 	return (
 		<div className="container">
 			<div className="display-1 text-center">
-				00:13:37
+				{time}
 			</div>
 		</div>
 	)
