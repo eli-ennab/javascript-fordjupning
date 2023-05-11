f81550311a28e317c55a728f7b14baad260dc363
Eli Ennab <elinaennab@gmail.com>
2023-05-11 15:00:21 +0200

    disable true on button if no input

--- 02-react-mini-workshop/src/components/AddTodoForm.tsx
+++ 02-react-mini-workshop/src/components/AddTodoForm.tsx
@@ -8,24 +8,25 @@ interface IProps {
 }
 
 const AddTodoForm: React.FC<IProps> = ({ onSubmit, onChange, newTodoTitle }) => {
-  return (
-	<form onSubmit={onSubmit} className="mb-3">
-		<div className="input-group">
-			<input
-				type="text"
-				className="form-control"
-				placeholder="Todo title"
-				onChange={e => onChange(e.target.value)}
-				value={newTodoTitle}
-			/>
+	return (
+		<form onSubmit={onSubmit} className="mb-3">
+			<div className="input-group">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Todo title"
+					onChange={e => onChange(e.target.value)}
+					value={newTodoTitle}
+				/>
 
-			<button
-				type="submit"
-				className="btn btn-success"
-			>Create</button>
-			</div>
-		</form>
-  )
+				<button
+					type="submit"
+					className="btn btn-success"
+					disabled={!newTodoTitle}
+				>Create</button>
+				</div>
+			</form>
+	)
 }
 
 export default AddTodoForm
