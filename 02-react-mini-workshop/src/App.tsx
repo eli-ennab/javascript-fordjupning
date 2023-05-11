588a04250e769c17af6b2c1b5f2e2b3c61c2a1fb
Eli Ennab <elinaennab@gmail.com>
2023-05-11 13:36:10 +0200

    added new mini-workshop, todos/components

---
+++ 02-react-mini-workshop/src/App.tsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import TodoListItem from './components/TodoListItem'
+import { Todo, TodoList } from './types'
+import './assets/scss/App.scss'
+
+function App() {
+	const [todos, setTodos] = useState<TodoList>([
+		{ title: "Make coffee", completed: true },
+		{ title: "Drink coffee", completed: false },
+		{ title: "Drink MOAR coffee", completed: false },
+		{ title: "Drink ALL THE coffee", completed: false },
+	])
+	const [newTodoTitle, setNewTodoTitle] = useState("")
+
+	const handleSubmit = (e: React.FormEvent) => {
+		// stop form from submitting
+		e.preventDefault()
+
+		// create a new todo and set a new todos state
+		const newTodo: Todo = {
+			title: newTodoTitle,
+			completed: false,
+		}
+		setTodos([...todos, newTodo])
+
+		// clear newTodoTitle state
+		setNewTodoTitle("")
+	}
+
+	const deleteTodo = (todoToDelete: Todo) => {
+		// set a new list of todos where the clicked todo is excluded
+		setTodos(todos.filter(todo => todo !== todoToDelete))
+	}
+
+	const toggleTodo = (todo: Todo) => {
+		todo.completed = !todo.completed
+		setTodos([...todos])
+	}
+
+	const unfinishedTodos = todos.filter(todo => !todo.completed)
+	const finishedTodos = todos.filter(todo => todo.completed)
+
+	return (
+		<div className="container">
+			<h1 className="mb-3">React Simple Todos</h1>
+
+			<form onSubmit={handleSubmit} className="mb-3">
+				<div className="input-group">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Todo title"
+						onChange={e => setNewTodoTitle(e.target.value)}
+						value={newTodoTitle}
+					/>
+
+					<button
+						type="submit"
+						className="btn btn-success"
+					>Create</button>
+				</div>
+			</form>
+
+			{todos.length > 0 && (
+				<>
+					<ul className="todolist">
+						{unfinishedTodos.map((todo, index) => (
+							<TodoListItem
+								onToggle={toggleTodo}
+								onDelete={deleteTodo}
+								todo={todo}
+								key={index}
+							/>
+						) )}
+					</ul>
+
+					<ul className="todolist">
+						{finishedTodos.map((todo, index) => (
+							<TodoListItem
+								onToggle={toggleTodo}
+								onDelete={deleteTodo}
+								todo={todo}
+								key={index}
+							/>
+						) )}
+					</ul>
+
+					<p className="status">
+						{finishedTodos.length} of {todos.length} todos completed
+					</p>
+				</>
+			)}
+
+			{todos.length === 0 && (
+				<p>Yayyy, you have 0 todos to do</p>
+			)}
+
+		</div>
+	)
+}
+
+export default App
