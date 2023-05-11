e4f559657d1b9dcda91d278e0802e1d80f987880
Eli Ennab <elinaennab@gmail.com>
2023-05-11 16:02:18 +0200

    collective alternative solutions to mini-workshop

--- 02-react-todos/src/App.tsx
+++ 02-react-todos/src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import TodoListItem from './components/TodoListItem'
 import { Todo, TodoList } from './types'
 import './assets/scss/App.scss'
-import TodoListItem from './components/TodoListItem'
+import TodoCounter from './components/TodoCounter'
+import AddNewTodoForm from './components/AddNewTodoForm'
 
 function App() {
 	const [todos, setTodos] = useState<TodoList>([
@@ -10,21 +12,9 @@ function App() {
 		{ title: "Drink MOAR coffee", completed: false },
 		{ title: "Drink ALL THE coffee", completed: false },
 	])
-	const [newTodoTitle, setNewTodoTitle] = useState("")
 
-	const handleSubmit = (e: React.FormEvent) => {
-		// stop form from submitting
-		e.preventDefault()
-
-		// create a new todo and set a new todos state
-		const newTodo: Todo = {
-			title: newTodoTitle,
-			completed: false,
-		}
-		setTodos([...todos, newTodo])
-
-		// clear newTodoTitle state
-		setNewTodoTitle("")
+	const addTodo = (todo: Todo) => {
+		setTodos([...todos, todo])
 	}
 
 	const deleteTodo = (todoToDelete: Todo) => {
@@ -40,26 +30,13 @@ function App() {
 	const unfinishedTodos = todos.filter(todo => !todo.completed)
 	const finishedTodos = todos.filter(todo => todo.completed)
 
+	// console.log("App rendering...")
+
 	return (
 		<div className="container">
 			<h1 className="mb-3">React Simple Todos</h1>
 
-			<form onSubmit={handleSubmit} className="mb-3">
-				<div className="input-group">
-					<input
-						type="text"
-						className="form-control"
-						placeholder="Todo title"
-						onChange={e => setNewTodoTitle(e.target.value)}
-						value={newTodoTitle}
-					/>
-
-					<button
-						type="submit"
-						className="btn btn-success"
-					>Create</button>
-				</div>
-			</form>
+			<AddNewTodoForm onAddTodo={addTodo} />
 
 			{todos.length > 0 && (
 				<>
@@ -85,9 +62,7 @@ function App() {
 						) )}
 					</ul>
 
-					<p className="status">
-						{finishedTodos.length} of {todos.length} todos completed
-					</p>
+					<TodoCounter finished={finishedTodos.length} total={todos.length} />
 				</>
 			)}
 
@@ -100,3 +75,4 @@ function App() {
 }
 
 export default App
+
