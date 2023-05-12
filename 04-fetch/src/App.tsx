e10afa09065c29861f8395512b33cd03ffa138ff
Eli Ennab <elinaennab@gmail.com>
2023-05-12 11:02:04 +0200

    fetch data two ways, with .then or async await

--- 04-fetch/src/App.tsx
+++ 04-fetch/src/App.tsx
@@ -1,19 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './assets/scss/App.scss'
+
+interface IResource {
+	id: number
+	title: string
+}
 
 function App() {
 	const [resource, setResource] = useState('posts')
-	const [data, setData] = useState([])
+	const [data, setData] = useState<IResource[]>([])
+
+	// fetch data with .then
+	// useEffect(() => {
+	// 	fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+	// 		.then(res => res.json())
+	// 		.then(payload => setData(payload))
+	// }, [resource])
+
+	// fetch data with async await
+	useEffect(() => {
+		const fetchData = async () => {
+			// fetch resource
+			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+
+			// parse response as json
+			const payload = await res.json() as IResource[]
+
+			// update data state with resource payload
+			setData(payload)
+		}
+
+		// call function
+		fetchData()
+	}, [resource])
 
 	return (
 		<div className="container">
 			<h1 className="mb-3">Fetch</h1>
 
 			<div className="d-flex justify-content-between">
-				<button onClick={() => setResource('albums')} className="btn btn-primary">Albums</button>
-				<button onClick={() => setResource('photos')} className="btn btn-success">Photos</button>
-				<button onClick={() => setResource('posts')} className="btn btn-warning">Posts</button>
-				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
+				<button onClick={() => setResource('albums')} className="btn btn-light">Albums</button>
+				<button onClick={() => setResource('photos')} className="btn btn-light">Photos</button>
+				<button onClick={() => setResource('posts')} className="btn btn-light">Posts</button>
+				<button onClick={() => setResource('todos')} className="btn btn-light">Todos</button>
 			</div>
 
 			{data && (
@@ -22,7 +51,7 @@ function App() {
 					<p>There are {data.length} {resource}.</p>
 					<ol>
 						{data.map(item => (
-							<li>{item.title}</li>
+							<li key={item.id}>{item.title}</li>
 						))}
 					</ol>
 				</>
