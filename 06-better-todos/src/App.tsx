b37943d3d99e2e39cd9f41ada31c0fe69eb8c4d9
Eli Ennab <elinaennab@gmail.com>
2023-05-26 16:52:43 +0200

    home page, todo page and navbar menu

--- 06-better-todos/src/App.tsx
+++ 06-better-todos/src/App.tsx
@@ -1,94 +1,22 @@
-import { useEffect, useState } from 'react'
-import { Todo, Todos } from './types'
 import Container from 'react-bootstrap/Container'
+import { Routes, Route } from 'react-router-dom'
 import './assets/scss/App.scss'
-import TodoCounter from './components/TodoCounter'
-import TodoList from './components/TodoList'
-import AddNewTodoForm from './components/AddNewTodoForm'
-import * as TodosAPI from './services/TodosAPI'
+import HomePage from './pages/HomePage'
+import TodosPage from './pages/TodosPage'
+import Navigation from './components/Navigation'
 
-function App() {
-	const [todos, setTodos] = useState<Todos>([])
-
-	// Get todos from api
-	const getTodos = async () => {
-		const data = await TodosAPI.getTodos()
-		setTodos(data)
-	}
-
-	// Create a new todo in the API
-	const addTodo = async (todo: Todo) => {
-		await TodosAPI.createTodo(todo)
-		getTodos()
-	}
-
-	// Delete a todo in the api
-	const deleteTodo = async (todo: Todo) => {
-		if (!todo.id) {
-			return
-		}
-
-		// Delete todo from the api
-		await TodosAPI.deleteTodo(todo.id)
-
-		// Get all the todos from the api
-		getTodos()
-	}
-
-	// Toggle the completed status of a todo in the api
-	const toggleTodo = async (todo: Todo) => {
-		if (!todo.id) {
-			return
-		}
-
-		// Update a todo in the api
-		await TodosAPI.updateTodo(todo.id, {
-			completed: !todo.completed
-		})
-
-		// Get all the todos from the api
-		getTodos()
-	}
-
-	// fetch todos when App is being mounted
-	useEffect(() => {
-		getTodos()
-	}, [])
-
-	const unfinishedTodos = todos.filter(todo => !todo.completed)
-	const finishedTodos = todos.filter(todo => todo.completed)
-
-	// console.log("App rendering...")
-
+const App = () => {
 	return (
-		<Container>
-			<h1 className="mb-3">React Simple Todos</h1>
+		<div id="App">
+			<Navigation />
 
-			<AddNewTodoForm onAddTodo={addTodo} />
-
-			{todos.length > 0 && (
-				<>
-					<TodoList
-						onToggle={toggleTodo}
-						onDelete={deleteTodo}
-						todos={unfinishedTodos}
-					/>
-
-					<TodoList
-						onToggle={toggleTodo}
-						onDelete={deleteTodo}
-						todos={finishedTodos}
-					/>
-
-					<TodoCounter finished={finishedTodos.length} total={todos.length} />
-				</>
-			)}
-
-			{todos.length === 0 && (
-				<p>Yayyy, you have 0 todos to do</p>
-			)}
-
-		</Container>
+			<Container className="py-3">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/todos" element={<TodosPage />} />
+				</Routes>
+			</Container>
+		</div>
 	)
 }
 
---
+++ 06-better-todos/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+	<>
+		<h1>Welcome to Better Todos!</h1>
+	</>
+  )
+}
+
+export default HomePage
