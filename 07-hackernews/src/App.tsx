c280def812c89deeefd5914840fbdffada8c7ee2
Eli Ennab <elinaennab@gmail.com>
2023-06-02 15:12:24 +0200

    enable route to search page

--- 07-hackernews/src/App.tsx
+++ 07-hackernews/src/App.tsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import SearchPage from './pages/SearchPage'
 import NotFound from './pages/NotFound'
 import Navigation from './components/Navigation'
 import './assets/scss/App.scss'
@@ -13,7 +14,7 @@ const App = () => {
 			<Container className="py-3">
 				<Routes>
 					<Route path="/" element={<HomePage />} />
-					{/* <Route path="/search" element={SearchHackerNews} /> */}
+					<Route path="/search" element={<SearchPage />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</Container>
