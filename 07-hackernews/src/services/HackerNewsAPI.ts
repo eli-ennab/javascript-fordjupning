2a4f97ac22c0ff2e0d58c055a691a4a1b0a524cf
Eli Ennab <elinaennab@gmail.com>
2023-06-04 22:07:46 +0200

    initial axios get requests

--- 07-hackernews/src/services/HackerNewsAPI.ts
+++ 07-hackernews/src/services/HackerNewsAPI.ts
@@ -1,5 +1,31 @@
 /**
- * Hacker News API
+ * Hacker News API service
+ *
+ * <https://hn.algolia.com/api>
  */
-// import axios from "axios"
 
+import axios from 'axios'
+
+/**
+ * Execute a HTTP GET request to an endpoint.
+ *
+ * @param {string} endpoint Endpoint to HTTP GET
+ * @returns Promise
+ */
+const get = async <T>(endpoint: string) => {
+	const response = await axios.get(endpoint)
+	return response.data as T
+}
+
+/**
+ * Search Hacker News stories
+ *
+ * @todo Replace any with correct type definition 😱!
+ *
+ * @param {string} query Search query to search for
+ * @param {number} page Page of search results to get
+ * @returns Promise
+ */
+export const search = async (query: string, page: number) => {
+	return get<any>(`/search?query=${query}&tags=story&page=${page}`)
+}
