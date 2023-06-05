915618735bc57c3687f577730191040d340f26f6
Eli Ennab <elinaennab@gmail.com>
2023-06-05 10:22:25 +0200

    created instance for axios and implemented types in the get searches

--- 07-hackernews/src/services/HackerNewsAPI.ts
+++ 07-hackernews/src/services/HackerNewsAPI.ts
@@ -5,6 +5,17 @@
  */
 
 import axios from 'axios'
+import { HN_SearchResponse } from '../types'
+
+// Create a new axios instance
+const instance = axios.create({
+	baseURL: "https://hn.algolia.com/api/v1",
+	timeout: 10000,
+	headers: {
+		"Content-Type": "application/json",
+		"Accept": "application/json",
+	}
+})
 
 /**
  * Execute a HTTP GET request to an endpoint.
@@ -13,7 +24,7 @@ import axios from 'axios'
  * @returns Promise
  */
 const get = async <T>(endpoint: string) => {
-	const response = await axios.get(endpoint)
+	const response = await instance.get(endpoint)
 	return response.data as T
 }
 
@@ -26,6 +37,19 @@ const get = async <T>(endpoint: string) => {
  * @param {number} page Page of search results to get
  * @returns Promise
  */
-export const search = async (query: string, page: number) => {
-	return get<any>(`/search?query=${query}&tags=story&page=${page}`)
+export const search = async (query: string, page = 0) => {
+	return get<HN_SearchResponse>(`/search?query=${query}&tags=story&page=${page}`)
+}
+
+/**
+ * Search Hacker News stories by date
+ *
+ * @todo Replace any with correct type definition 😱!
+ *
+ * @param {string} query Search query to search for
+ * @param {number} page Page of search results to get
+ * @returns Promise
+ */
+export const searchByDate = async (query: string, page = 0) => {
+	return get<HN_SearchResponse>(`/search_by_date?query=${query}&tags=story&page=${page}`)
 }
