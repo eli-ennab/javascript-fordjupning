bb9fde6ba4b394c60636b09daf5f373667bb1241
Eli Ennab <elinaennab@gmail.com>
2023-06-08 11:42:34 +0200

    toggle set dark or light mode in nav

--- 07-hackernews/src/components/Navigation.tsx
+++ 07-hackernews/src/components/Navigation.tsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { NavLink, Link } from 'react-router-dom'
+import { ThemeContext } from '../contexts/ThemeProvider'
+import Button from 'react-bootstrap/Button'
 
 const Navigation = () => {
+	const { isLightMode, toggleTheme } = useContext(ThemeContext)
+
 	return (
 			<Navbar bg="dark" variant="dark" expand="md">
 				<Container>
@@ -13,6 +18,9 @@ const Navigation = () => {
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
 							<Nav.Link as={NavLink} end to="/search">Search Hacker News</Nav.Link>
+							<Button variant="light" onClick={toggleTheme}>
+								{isLightMode ? 'set dark mode' : 'set light mode'}
+							</Button>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
