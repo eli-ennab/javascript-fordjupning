f277e0d1c18b6ac4f70bb9adb774181058a0d99b
Eli Ennab <elinaennab@gmail.com>
2023-06-13 10:01:14 +0200

    changed name on context

--- 07-hackernews/src/main.tsx
+++ 07-hackernews/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import ThemeProvider from './contexts/ThemeProvider.tsx'
+import ThemeProvider from './contexts/ThemeContextProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
