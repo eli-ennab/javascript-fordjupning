7c7bfbd4cb368693967996dc5994715d701f2eaf
Eli Ennab <elinaennab@gmail.com>
2023-06-15 11:10:08 +0200

    examples of what react query can do, i like

--- 08-react-query-sneak-peek/src/pages/ICanHazDadJokePage.tsx
+++ 08-react-query-sneak-peek/src/pages/ICanHazDadJokePage.tsx
@@ -1,25 +1,43 @@
+import { useQuery } from '@tanstack/react-query'
 import Alert from "react-bootstrap/Alert"
 import Button from "react-bootstrap/Button"
 import Spinner from "react-bootstrap/Spinner"
+import { getRandomDadJoke } from '../services/ICanHazDadJokeAPI'
 
 const ICanHazDadJokePage = () => {
+	// const jokeQuery = useQuery(["random-dad-joke"], getRandomDadJoke)
+	const {
+		data,
+		isError,
+		isLoading,
+		isFetching,
+		isStale,
+		isSuccess,
+		status
+	} = useQuery(["random-dad-joke"], getRandomDadJoke)
+
 	return (
 		<>
 			<h1>Random Dad Joke</h1>
 
 			<pre className="bg-light py-2 px-3">
-				i can haz jokes
+				isError: {isError ? "true" : "false"}<br />
+				isLoading: {isLoading ? "true" : "false"}<br />
+				isFetching: {isFetching ? "true" : "false"}<br />
+				isStale: {isStale ? "true" : "false"}<br />
+				isSuccess: {isSuccess ? "true" : "false"}<br />
+				status: {status}
 			</pre>
 
-			{false && <Spinner animation="border" variant="secondary" />}
+			{isLoading && <Spinner animation="border" variant="secondary" />}
 
-			{false && <Alert variant="warning">ERROR! ERROR! ERROR!</Alert>}
+			{isError && <Alert variant="warning">ERROR! ERROR! ERROR!</Alert>}
 
 			<div>
 				{true && (
 					<>
 						<p className="display-5 text-center my-5">
-							JOKE
+							{data?.joke}
 						</p>
 					</>
 				)}
