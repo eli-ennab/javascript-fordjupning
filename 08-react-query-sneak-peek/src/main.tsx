9a7928dd3ebe7c3db215287d34222a1f2df29306
Eli Ennab <elinaennab@gmail.com>
2023-06-15 11:09:41 +0200

    implement react query and make default options to not refetch on window focus

--- 08-react-query-sneak-peek/src/main.tsx
+++ 08-react-query-sneak-peek/src/main.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ThemeContextProvider from './contexts/ThemeContextProvider.tsx'
 import App from './App.tsx'
+
+// create a React Query Client and set default behaviour and options
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		}
+	}
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 
-		<BrowserRouter>
-			<ThemeContextProvider>
-				<App />
-			</ThemeContextProvider>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<ThemeContextProvider>
+					<App />
+				</ThemeContextProvider>
+			</BrowserRouter>
+		</QueryClientProvider>
 
 	</React.StrictMode>,
 )
