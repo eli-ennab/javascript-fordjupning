fd135cf8de2fa16d0c24a0087327eaab7ae22b42
Eli Ennab <elinaennab@gmail.com>
2023-08-15 11:48:48 +0200

    react router, homepage and not found page

--- 09-react-query/src/App.tsx
+++ 09-react-query/src/App.tsx
@@ -1,13 +1,20 @@
 import Container from 'react-bootstrap/Container'
-import './assets/scss/App.scss'
 import Navigation from './pages/partials/Navigation'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import PageNotFound from './pages/PageNotFound'
+
+import './assets/scss/App.scss'
 
 const App = () => {
 	return (
 		<div id="App">
 			<Navigation />
 			<Container className="py-3">
-				<h1>I LUV REACT QUERY</h1>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
 			</Container>
 		</div>
 	)
---
+++ 09-react-query/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+	return (
+		<>
+			<h1>I LUV REACT QUERY</h1>
+
+			<p>Welcome to my cat appreciation page</p>
+		</>
+	)
+}
+
+export default HomePage
