1ca3ccb1bfdfe9573ca9d26b929f738d5d569599
Eli Ennab <elinaennab@gmail.com>
2023-08-15 11:56:32 +0200

    Link and NavLink from react-router-dom

--- 09-react-query/src/pages/partials/Navigation.tsx
+++ 09-react-query/src/pages/partials/Navigation.tsx
@@ -1,16 +1,17 @@
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navigation = () => {
 	return (
 		<Navbar bg="dark" data-bs-theme="dark" expand="md">
 			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link as={NavLink} to="/random-cat">Random cat</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
