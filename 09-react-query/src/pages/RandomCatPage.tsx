b77508ae33c710baea66af5c7331cf947bfa122f
Eli Ennab <elinaennab@gmail.com>
2023-08-16 09:20:01 +0200

    implemented teacher's catspinner from yesterdays lesson

--- 09-react-query/src/pages/RandomCatPage.tsx
+++ 09-react-query/src/pages/RandomCatPage.tsx
@@ -3,6 +3,7 @@ import { getRandomCat } from './../services/TheCatAPI'
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 import Image from 'react-bootstrap/Image'
+import CatSpinner from './components/CatSpinner'
 
 const RandomCatPage = () => {
 
@@ -30,7 +31,7 @@ const RandomCatPage = () => {
 				</Button>
 			</div>
 
-			{ isFetching && <p>Loading a lil' cat img...</p>}
+			{isFetching && <CatSpinner />}
 
 			{ data &&
 				data.map(data => <Image key={data.id} src={data.url} fluid />)
