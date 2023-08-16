1e03aa8f8adcf276c7e0099e820eaf78cc09b482
Eli Ennab <elinaennab@gmail.com>
2023-08-16 10:29:41 +0200

    preparing for get random cat by breed

--- 09-react-query/src/services/TheCatAPI.ts
+++ 09-react-query/src/services/TheCatAPI.ts
@@ -34,6 +34,24 @@ export const getRandomCatImage = async () => {
 }
 
 /**
+ * Get a random cat image of a bengal cat
+ */
+export const getRandomBengalCatImage = async () => {
+	const data = await get<CatAPI_ImageResponse>("/images/search?breed_ids=beng")
+
+	return data[0]
+}
+
+/**
+ * Get a random cat image of a specific breed
+ */
+export const getRandomCatImageByBreed = async (breed: string) => {
+	const data = await get<CatAPI_ImageResponse>("/images/search?breed_ids=" + breed)
+
+	return data[0]
+}
+
+/**
  * Get random cat images
  */
 export const getRandomCatImages = async (qty = 1) => {
