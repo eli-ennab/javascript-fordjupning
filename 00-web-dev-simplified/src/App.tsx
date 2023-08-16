d669e67fe21bad51b80efed27327cad5742293cd
Eli Ennab <elinaennab@gmail.com>
2023-08-16 10:06:52 +0200

    useQuery for get POSTS and render them + loading and error msg

--- 00-web-dev-simplified/src/App.tsx
+++ 00-web-dev-simplified/src/App.tsx
@@ -1,8 +1,31 @@
-function App() {
+import { useQuery, useMutation } from '@tanstack/react-query'
 
-  return (
-	<h1>TanStack Query</h1>
-  )
+const POSTS =  [
+	{ id: 1, title: "Post 1"},
+	{ id: 2, title: "Post 2"}
+]
+
+function App() {
+	const postsQuery = useQuery({
+		queryKey: ['posts'],
+		queryFn: () => wait(1000).then(() => [...POSTS])
+	})
+
+	if (postsQuery.isLoading) return <h1>Loading...</h1>
+
+	if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>
+
+	return (
+		<div>
+			{postsQuery.data.map(post => (
+				<div key={post.id}>{post.title}</div>
+			))}
+		</div>
+	)
+}
+
+function wait(duration: number) {
+	return new Promise(resolve => setTimeout(resolve, duration))
 }
 
 export default App
