6bc3d3ab1e2e4719582e95218c4c0b3896d33b97
Eli Ennab <elinaennab@gmail.com>
2023-08-18 13:07:01 +0200

    install react query and new query client

--- 10-better-todos/src/main.tsx
+++ 10-better-todos/src/main.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 15, // 15 minutes
+			cacheTime: 1000 * 60 * 60, // 1 hour
+		}
+	}
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</React.StrictMode>,
 )
