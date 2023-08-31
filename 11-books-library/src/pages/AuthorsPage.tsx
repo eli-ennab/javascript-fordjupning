f940d5a5dc34e23821dbb59055b965aecfff8826
Eli Ennab <elinaennab@gmail.com>
2023-08-31 09:15:33 +0200

    clickable links to authors/:1

--- 11-books-library/src/pages/AuthorsPage.tsx
+++ 11-books-library/src/pages/AuthorsPage.tsx
@@ -1,12 +1,24 @@
 import { createColumnHelper } from '@tanstack/react-table'
+import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom'
 import WarningAlert from '../components/alerts/WarningAlert'
-import PageTransition from '../components/animations/PageTransition'
-import BSAuthorTable from '../components/BSAuthorTable'
-import CreateAuthorForm from '../components/forms/CreateAuthorForm'
-// import TanstackSortableTable from '../components/TanstackSortableTable'
+import TanstackSortableTable from '../components/TanstackSortableTable'
 import useAuthors from '../hooks/useAuthors'
 import { Author } from '../types/BooksAPI.types'
-import Card from 'react-bootstrap/Card'
+import CreateAuthorForm from '../components/forms/CreateAuthorForm'
+
+/*
+const columns: ColumnDef<Author>[] = [
+	{
+		accessorKey: 'name',
+		header: 'Name',
+	},
+	{
+		accessorKey: 'date_of_birth',
+		header: 'Date of birth',
+	},
+]
+*/
 
 const columnHelper = createColumnHelper<Author>()
 
@@ -19,35 +31,37 @@ const columns = [
 		columns: [
 			columnHelper.accessor('name', {
 				header: 'Name',
+				cell: props => (
+					<Link to={`/authors/${props.row.original.id}`}>
+						{props.getValue()}
+					</Link>
+				)
 			}),
 			columnHelper.accessor('date_of_birth', {
 				header: 'Date of birth',
 			}),
 		],
 	}),
-	/*
 	columnHelper.display({
 		id: 'actions',
-		cell: (props) => (
+		cell: props => (
 			<div className="flex justify-end">
-				<button className="btn btn-sm btn-primary">View</button>
-				<button className="btn btn-sm btn-warning ml-2">Edit</button>
+				<Link className="btn btn-primary btn-sm" to={`/authors/${props.row.original.id}`}>View</Link>
 			</div>
 		),
 	})
-	*/
 ]
 
 const AuthorsPage = () => {
 	const { data: authors, isError, isLoading } = useAuthors()
 
 	return (
-		<PageTransition page="authors-page">
+		<>
 			<h1 className="mb-3">Authors</h1>
 
 			{isError && (
 				<WarningAlert>
-					Something went wrong.
+					An terrible, inexplicable error occurred while fetching authors. It wasn't me!
 				</WarningAlert>
 			)}
 
@@ -55,10 +69,9 @@ const AuthorsPage = () => {
 				<p>Loading authors...</p>
 			)}
 
-			{/* {authors && <TanstackSortableTable columns={columns} data={authors} />} */}
-			{authors && <BSAuthorTable authors={authors} />}
+			{authors && <TanstackSortableTable columns={columns} data={authors} />}
 
-			<hr className="mb-5"></hr>
+			<hr className="mb-5" />
 
 			<Card>
 				<Card.Body>
@@ -66,8 +79,9 @@ const AuthorsPage = () => {
 					<CreateAuthorForm />
 				</Card.Body>
 			</Card>
-		</PageTransition>
+		</>
 	)
 }
 
 export default AuthorsPage
+
