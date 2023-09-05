2b50ed5b0baf6f7150d144ca990141058e2f3c4d
Eli Ennab <elinaennab@gmail.com>
2023-09-05 15:17:57 +0200

    fucked up firebase.ts by not exporting it but now its back to normal

--- 12-firebase-todos/src/services/firebase.ts
+++ 12-firebase-todos/src/services/firebase.ts
@@ -1,21 +1,25 @@
 import { initializeApp } from "firebase/app"
-import { CollectionReference, collection, getFirestore, DocumentData } from "firebase/firestore"
+import { CollectionReference, collection, DocumentData, getFirestore } from "firebase/firestore"
 import { Todo } from "../types/Todo.types"
 
-// Firebase configuration
+// Your web app's Firebase configuration
+
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
 	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
 	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
-// A helper to add the type to the db response
+// Get Firestore instance
+export const db = getFirestore(app)
+
+// This is just a helper to add the type to the db responses
 const createCollection = <T = DocumentData>(collectionName: string) => {
 	return collection(db, collectionName) as CollectionReference<T>
 }
@@ -23,5 +27,4 @@ const createCollection = <T = DocumentData>(collectionName: string) => {
 // Export collection references
 export const todosCol = createCollection<Todo>("todos")
 
-// Get Firestore instance
-export const db = getFirestore(app)
+export default app
