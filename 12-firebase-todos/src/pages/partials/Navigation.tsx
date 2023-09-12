d740dd91cb18275414cdef672d1d15d3446cb9a0
Eli Ennab <elinaennab@gmail.com>
2023-09-12 14:28:19 +0200

    showing routes depending on if currentUser or not

--- 12-firebase-todos/src/pages/partials/Navigation.tsx
+++ 12-firebase-todos/src/pages/partials/Navigation.tsx
@@ -2,8 +2,11 @@ import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { NavLink, Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 const Navigation = () => {
+	const { currentUser } = useAuth()
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="sm">
 			<Container>
@@ -12,10 +15,16 @@ const Navigation = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/signup">Signup</Nav.Link>
-						<Nav.Link as={NavLink} end to="/login">Login</Nav.Link>
-						<Nav.Link as={NavLink} end to="/logout">Logout</Nav.Link>
-						<Nav.Link as={NavLink} end to="/todos">Todos</Nav.Link>
+						{ currentUser ? (<>
+							{/* User is logged in */}
+							<Nav.Link as={NavLink} end to="/todos">Todos</Nav.Link>
+							<Nav.Link as={NavLink} end to="/logout">Logout</Nav.Link>
+						</>) : (<>
+							{/* No user is logged in */}
+							<Nav.Link as={NavLink} end to="/signup">Signup</Nav.Link>
+							<Nav.Link as={NavLink} end to="/login">Login</Nav.Link>
+						</>)
+						}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
