244cb669c38238e40c5b96084d6dd198a27632d2
Eli Ennab <elinaennab@gmail.com>
2023-09-13 09:31:43 +0200

    types for updating form data

--- 12-firebase-todos/src/types/User.types.ts
+++ 12-firebase-todos/src/types/User.types.ts
@@ -10,6 +10,9 @@ export type SignUpCredentials = {
 }
 
 export type UpdateProfileFormData = {
-	// set type
+	email?: string
+	password?: string
+	// displayName?: string
+	// photoUrl?: string
 }
 
