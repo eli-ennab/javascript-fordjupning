cd01b5dfe777dd69ee435f2147effdeb7109c80d
Eli Ennab <elinaennab@gmail.com>
2023-09-13 12:01:34 +0200

    all the logic for updating user in update profile page. bug: no reload of user...

--- 12-firebase-todos/src/pages/UpdateProfile.tsx
+++ 12-firebase-todos/src/pages/UpdateProfile.tsx
@@ -13,9 +13,18 @@ import { UpdateProfileFormData } from '../types/User.types'
 
 const UpdateProfile = () => {
 	const [errorMessage, setErrorMessage] = useState<string | null>(null)
+	// const [errorMessage, setErrorMessage] = useState([]) // make error an array later
 	const [loading, setLoading] = useState(false)
 	const { handleSubmit, register, watch, formState: { errors } } = useForm<UpdateProfileFormData>()
-	const { signup } = useAuth()
+	const {
+		currentUser,
+		setEmail,
+		setDisplayName,
+		setPhotoUrl,
+		userEmail,
+		userPhotoUrl,
+		userDisplayName,
+} = useAuth()
 
 	// Watch the current value of `password` form field
 	const passwordRef = useRef("")
@@ -25,23 +34,39 @@ const UpdateProfile = () => {
 		// Clear any previous error state
 		setErrorMessage(null)
 
+		if (!currentUser) {
+			throw new Error("No user")
+		}
+
 		// Update user profile
 		try {
 			// Disable update-button while update is in progress
 			setLoading(true)
 
 			// Update displayName *ONLY* if it has changed
+			if (userDisplayName !== data.displayName) {
+				await setDisplayName(currentUser, data.displayName)
+			}
+
+			// Update photoURL *ONLY* if it has changed
+			if (userPhotoUrl !== data.photoUrl) {
+				await setPhotoUrl(currentUser, data.photoUrl)
+			}
 
 			// Update email *ONLY* if it has changed
+			if (userEmail !== data.email) {
+				await setEmail(currentUser, data.email)
+			}
 
 			// Update password *ONLY* if the user has provided a new password to set
 
 			// Reload user data
+			// userReload()
 
 			// Show success toast 🥂
 
 			// Enable update-button again
-
+			setLoading(false)
 		} catch (error) {
 			if (error instanceof FirebaseError) {
 				setErrorMessage(error.message)
@@ -67,34 +92,44 @@ const UpdateProfile = () => {
 									Fill the displayName, photoURL and email form fields with their current value!
 								*/}
 								<Form.Group controlId="displayName" className="mb-3">
-									<Form.Label>Name</Form.Label>
+									<Form.Label>Display Name</Form.Label>
 									<Form.Control
-										placeholder="Sean Banan"
+										placeholder={userDisplayName ? userDisplayName : 'set display name'}
 										type="text"
+										{...register('displayName')}
 									/>
 								</Form.Group>
 
 								<Form.Group controlId="photoURL" className="mb-3">
 									<Form.Label>Photo URL</Form.Label>
 									<Form.Control
-										placeholder="https://www.chiquita.com/Bananana.jpg"
+										placeholder={userPhotoUrl ? userPhotoUrl : 'set photo url'}
 										type="url"
+										// value={currentUser?.photoURL}
+										{...register('photoUrl')}
 									/>
 								</Form.Group>
 
 								<Form.Group controlId="email" className="mb-3">
 									<Form.Label>Email</Form.Label>
 									<Form.Control
-										placeholder="snelhest2000@horsemail.com"
+										placeholder={userEmail ? userEmail : 'youremail@email.com'}
 										type="email"
+										{...register('email')}
 									/>
 								</Form.Group>
 
-								<Form.Group controlId="password" className="mb-3">
+								{/* <Form.Group controlId="password" className="mb-3">
 									<Form.Label>Password</Form.Label>
 									<Form.Control
 										type="password"
 										autoComplete="new-password"
+										{...register('password', {
+											minLength: {
+												value: 3,
+												message: "Please enter at least 3 characters"
+											},
+										})}
 									/>
 									<Form.Text>At least 6 characters</Form.Text>
 								</Form.Group>
@@ -104,8 +139,17 @@ const UpdateProfile = () => {
 									<Form.Control
 										type="password"
 										autoComplete="off"
+										{...register('passwordConfirm', {
+											minLength: {
+												value: 3,
+												message: "Please enter at least 3 characters"
+											},
+											validate: (value) => {
+												return value === passwordRef.current || "The passwords does not match 🤦🏼‍♂️"
+											}
+										})}
 									/>
-								</Form.Group>
+								</Form.Group> */}
 
 								<Button
 									disabled={loading}
@@ -117,10 +161,8 @@ const UpdateProfile = () => {
 										: "Save"}
 								</Button>
 							</Form>
-
 						</Card.Body>
 					</Card>
-
 				</Col>
 			</Row>
 		</Container>
