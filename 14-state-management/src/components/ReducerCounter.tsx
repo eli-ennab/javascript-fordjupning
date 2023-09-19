f6ef8d6f76998aa3e552b548f043fde2605946f1
Eli Ennab <elinaennab@gmail.com>
2023-09-19 11:51:00 +0200

    reducer counter with Reducer and useReducer

--- 14-state-management/src/components/ReducerCounter.tsx
+++ 14-state-management/src/components/ReducerCounter.tsx
@@ -1,14 +1,59 @@
+import { Reducer, useReducer } from 'react'
 import Button from 'react-bootstrap/Button'
 
+type PointsState = {
+	points: number
+}
+
+type PointsAction = {
+	type: "increment" | "decrement"
+}
+
+const initialState: PointsState = {
+	points: 0
+}
+
+const pointsReducer = (state: PointsState, action: PointsAction) => {		// takes current state and what (action) we want to do with the state
+	// state = current state, action = { type: "increment" }
+	// console.log("Current state:", state)
+	// console.log("Action:", action)
+
+	switch (action.type) {
+		case "decrement":
+			console.log("Would decrement points", action)
+			return {
+				points: state.points - 1	// returning a new state
+			}
+
+		case "increment":
+			console.log("Would increment points", action)
+			return {
+				points: state.points + 1	// returning a new state
+			}
+
+		default:
+			return state
+	}
+}
+
 const ReducerCounter = () => {
+	const [state, dispatch] = useReducer<Reducer<PointsState, PointsAction>>(pointsReducer, initialState)	// dispatch modifies state
 
 	return (
 		<div className="counter">
-			<Button variant="warning" onClick={() => null}>-</Button>
+			<Button
+				variant="warning"
+				onClick={() => dispatch({ type: "decrement" })}>
+					-
+			</Button>
 
-			<span className="points">{null}</span>
+			<span className="points">{state.points}</span>
 
-			<Button variant="success" onClick={() => null}>+</Button>
+			<Button
+				variant="success"
+				onClick={() => dispatch({ type: "increment" })}>
+					+
+			</Button>
 		</div>
 	)
 }
