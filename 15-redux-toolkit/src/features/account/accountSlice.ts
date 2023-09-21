daa43dbbfb04c51c3ed03d66b6b08f879679a649
Eli Ennab <elinaennab@gmail.com>
2023-09-21 11:13:18 +0200

    added accountslice and added to store

---
+++ 15-redux-toolkit/src/features/account/accountSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Account } from '../../types/Account.types'
+
+const initialState: Account = {
+	balance: 1337
+}
+
+export const accountSlice = createSlice({
+	name: "account",
+	initialState: initialState,
+	reducers: {
+		deposit: (state, action: PayloadAction<number>) => {
+			state.balance += action.payload
+		},
+		withdraw: (state, action: PayloadAction<number>) => {
+			state.balance -= action.payload
+		},
+	}
+})
+
+export default accountSlice.reducer
